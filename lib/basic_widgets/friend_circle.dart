da744b3d6565959cacd0d510fffff3693704ce83
SmallStoneSK <575913857@qq.com>
2019-07-06 21:21:46 +0800

    [add] 添加基础组件示例 - FriendCircle

---
+++ lib/basic_widgets/friend_circle.dart
@@ -0,0 +1,179 @@
+import 'package:flutter/material.dart';
+
+class FriendCircle extends StatelessWidget {
+  final FriendCircleViewModel data;
+
+  const FriendCircle({
+    Key key,
+    this.data,
+  }) : super(key: key);
+
+  Widget renderComments() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      padding: EdgeInsets.all(10),
+      color: Color(0xFFF3F3F5),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: this.data.comments.map((comment) {
+          return Text.rich(
+            TextSpan(
+              style: TextStyle(
+                fontSize: 15,
+                color: Color(0xFF333333),
+              ),
+              children: [
+                TextSpan(
+                  text: comment.fromUser,
+                  style: TextStyle(
+                    fontWeight: FontWeight.w500,
+                    color: Color(0xFF636F80),
+                  ),
+                ),
+                TextSpan(text: '：${comment.content}'),
+              ]..insertAll(1, comment.source ? [TextSpan()] : [
+                  TextSpan(text: ' 回复 '),
+                  TextSpan(
+                    text: comment.toUser,
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      color: Color(0xFF636F80),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 16),
+      padding: EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        border: Border(
+          top: BorderSide(width: 1, color: Color(0xFFDDDDDD)),
+          bottom: BorderSide(width: 1, color: Color(0xFFDDDDDD)),
+        ),
+      ),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: <Widget>[
+          ClipRRect(
+            borderRadius: BorderRadius.circular(6),
+            child: Image.network(
+              this.data.userImgUrl,
+              width: 50,
+              height: 50,
+            ),
+          ),
+          Padding(padding: EdgeInsets.only(left: 12)),
+          Expanded(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              mainAxisSize: MainAxisSize.min,
+              children: <Widget>[
+                Text(
+                  this.data.userName,
+                  style: TextStyle(
+                    fontSize: 19,
+                    fontWeight: FontWeight.bold,
+                    color: Color(0xFF636F80),
+                  ),
+                ),
+                Padding(padding: EdgeInsets.only(top: 6)),
+                Text(
+                  this.data.saying,
+                  style: TextStyle(
+                    fontSize: 15,
+                    height: 0.8,
+                    color: Color(0xFF333333),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(vertical: 10),
+                  child: Image.network(
+                    this.data.pic,
+                    fit: BoxFit.fitWidth,
+                  ),
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      this.data.publishTime,
+                      style: TextStyle(
+                        fontSize: 13,
+                        color: Color(0xFF999999),
+                      ),
+                    ),
+                    Icon(
+                      Icons.comment,
+                      size: 16,
+                      color: Color(0xFF999999),
+                    ),
+                  ],
+                ),
+                this.renderComments(),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class FriendCircleViewModel {
+  /// 用户名
+  final String userName;
+
+  /// 用户头像地址
+  final String userImgUrl;
+
+  /// 说说
+  final String saying;
+
+  /// 图片
+  final String pic;
+
+  ///发布时间
+  final String publishTime;
+
+  /// 评论内容
+  final List<Comment> comments;
+
+  const FriendCircleViewModel({
+    this.userName,
+    this.userImgUrl,
+    this.saying,
+    this.pic,
+    this.publishTime,
+    this.comments,
+  });
+}
+
+class Comment {
+  /// 是否发起人
+  final bool source;
+
+  /// 评论者
+  final String fromUser;
+
+  /// 被评论者
+  final String toUser;
+
+  // 评论内容
+  final String content;
+
+  const Comment({
+    this.source,
+    this.fromUser,
+    this.toUser,
+    this.content,
+  });
+}
--- lib/basic_widgets/index.dart
+++ lib/basic_widgets/index.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 
 import 'pet_card.dart';
 import 'credit_card.dart';
+import 'friend_circle.dart';
 
 const PetCardViewModel petCardData = PetCardViewModel(
   coverUrl: 'https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=1425538345,901220022&fm=26&gp=0.jpg',
@@ -25,6 +26,58 @@ const CreditCardViewModel creditCardData = CreditCardViewModel(
   validDate: '10/27',
 );
 
+const FriendCircleViewModel friendCircleData = FriendCircleViewModel(
+  userName: '小石头',
+  userImgUrl: 'https://mirror-gold-cdn.xitu.io/168e089a9fc6d1a7a5c?imageView2/1/w/100/h/100/q/85/format/webp/interlace/1',
+  saying: '听说Flutter很🔥，我也来凑热闹，github上建了一个仓库，欢迎来撩~✌✌✌',
+  pic: 'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1562427219775&di=0ffec9a8d7fd40f4b7b4260f2fefe910&imgtype=0&src=http%3A%2F%2Fimg3.mukewang.com%2F5c18cf540001ac8206000338.jpg',
+  publishTime: '2小时前',
+  comments: [
+    Comment(
+      source: true,
+      fromUser: '若海',
+      toUser: '小石头',
+      content: '性能如何？'
+    ),
+    Comment(
+      source: false,
+      fromUser: '小石头',
+      toUser: '若海',
+      content: '性能不错，但是开发体验感觉差一点。。。'
+    ),
+    Comment(
+      source: false,
+      fromUser: '若海',
+      toUser: '小石头',
+      content: '周末我也试试，嘻嘻~'
+    ),
+    Comment(
+      source: true,
+      fromUser: '大佬',
+      toUser: '小石头',
+      content: '卧槽，求求你别学了'
+    ),
+    Comment(
+      source: true,
+      fromUser: '产品',
+      toUser: '小石头',
+      content: '工作量不饱和啊你这是！'
+    ),
+    Comment(
+      source: false,
+      fromUser: '小石头',
+      toUser: '大佬',
+      content: '卧槽，大佬别闹，学不动了。。。'
+    ),
+    Comment(
+      source: false,
+      fromUser: '小石头',
+      toUser: '产品',
+      content: '不不不，你的需求都已经完成了~！'
+    ),
+  ],
+);
+
 class BasicWidgetsDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -38,6 +91,7 @@ class BasicWidgetsDemo extends StatelessWidget {
         children: <Widget>[
           PetCard(data: petCardData),
           CreditCard(data: creditCardData),
+          FriendCircle(data: friendCircleData),
         ],
       ),
     );
